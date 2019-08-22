5ed7fdaf67a705a02d3b0d1e47e6306a32845e3d
lucasschn <lucas.schneeberger@gmail.com>
2019-08-22 18:19:58 +0200

    avg hover func added and integrated in analog. Payload needs testing

--- Vibrations/analog.py
+++ Vibrations/analog.py
@@ -1,12 +1,56 @@
 import pyulog
 import numpy as np
 import pandas as pd
+from pyulgresample.ulogdataframe import DfUlg, TopicMsgs
 
 def ampspectrum(x):
     """ Returns the magnitudes of the fft for a real x"""
     complex_spectrum = rfft(x)
     X = np.abs(complex_spectrum)
     return X
+
+def avghover(path):
+    topic_list = []
+    topic_list.append('vehicle_status')
+    topic_list.append('actuator_outputs')
+    topic_list.append('manual_control_setpoint')
+
+    dfulg = DfUlg.create(path,topic_list)
+
+    msglist = []
+    for k in range(6):
+        msglist.append(f'F_output_{k}')
+
+    rpm1 = dfulg.df[f'T_actuator_outputs_0__{msglist[0]}']
+    rpm2 = dfulg.df[f'T_actuator_outputs_0__{msglist[1]}']
+    rpm3 = dfulg.df[f'T_actuator_outputs_0__{msglist[2]}']
+    rpm4 = dfulg.df[f'T_actuator_outputs_0__{msglist[3]}']
+    rpm5 = dfulg.df[f'T_actuator_outputs_0__{msglist[4]}']
+    rpm6 = dfulg.df[f'T_actuator_outputs_0__{msglist[5]}']
+
+    stick_in_x = dfulg.df['T_manual_control_setpoint_0__F_x']
+    stick_in_y = dfulg.df['T_manual_control_setpoint_0__F_y']
+    stick_in_z = dfulg.df['T_manual_control_setpoint_0__F_z']
+
+    navstate = dfulg.df['T_vehicle_status_0__F_nav_state']
+
+
+    hovering_bool=[]
+
+    for k in range(len(navstate)):
+        hovering_bool.append(ishover(navstate[k],stick_in_x[k],stick_in_y[k],stick_in_z[k]))
+
+    average_hovering_rpm1 = np.mean(rpm1[hovering_bool])
+    average_hovering_rpm2 = np.mean(rpm2[hovering_bool])
+    average_hovering_rpm3 = np.mean(rpm3[hovering_bool])
+    average_hovering_rpm4 = np.mean(rpm4[hovering_bool])
+    average_hovering_rpm5 = np.mean(rpm5[hovering_bool])
+    average_hovering_rpm6 = np.mean(rpm6[hovering_bool])
+
+    average_hovering_rpm=np.mean([average_hovering_rpm1,average_hovering_rpm2,average_hovering_rpm3,average_hovering_rpm4,average_hovering_rpm5,average_hovering_rpm6])
+
+    return average_hovering_rpm
+
 
 def ishover(navstate,stick_in_x,stick_in_y,stick_in_z):
     """ Takes navstate and stick inputs at a certain timestep as arguments. Returns True is the drone is hovering at this moment, False elsewise"""
@@ -39,12 +83,13 @@ def logextract(path,topic_list):
                     continue
                 else:                     
                     data_ao = topic.data
-                    # columns = ['motor 1','motor 2','motor 3','motor 4','motor 5','motor 6']
+                    time_ao = data_ao['timestamp']
+                    columns = ['motor 1','motor 2','motor 3','motor 4','motor 5','motor 6']
                     rpm=[]
                     for k in range(np.unique(data_ao['noutputs']).item()):
                         rpm.insert(k,data_ao[f'output[{k}]'])
-                    # rpm_df = pd.DataFrame(rpm,range(len(time_ao)),columns)
-                    info.update({'rpm': rpm})
+                    rpm_df = pd.DataFrame(np.array([rpm[0],rpm[1],rpm[2],rpm[3],rpm[4],rpm[5]]).transpose(),index=range(len(time_ao)),columns=columns)
+                    info.update({'time_ao':time_ao,'rpm': rpm,'rpm_df': rpm_df})
         elif topic.name == 'vehicle_local_position_setpoint':
             data_vlps = topic.data
             vert_thrust = data_vlps['thrust[2]']
@@ -61,6 +106,5 @@ def logextract(path,topic_list):
             stick_in_x = data_mcs['x']
             stick_in_y = data_mcs['y']
             stick_in_z = data_mcs['z']
-            info.update({'stick_in_x': stick_in_x, 'stick_in_y': stick_in_y, 'stick_in_z': stick_in_z})
+            info.update({'time_mcs':time_mcs,'stick_in_x': stick_in_x, 'stick_in_y': stick_in_y, 'stick_in_z': stick_in_z})
     return info
-
--- Vibrations/payload.py
+++ Vibrations/payload.py
@@ -1,33 +1,48 @@
-from analog import logextract as logextract, ishover as ishover
+from analog import avghover as avghover
 import numpy as np
-import pandas as  pd
+import os
+import csv 
+
+def addrpm(log_file,rpm):
+    """ Adds the average hovering rpm results of the file log_file to the file avg_rpm.csv """
+    path2csv = 'avg_rpm.csv'
+    try:
+        csv_file = open(path2csv)
+        print(f'{path2csv} has been succesfully opened.')
+        if csv_file.readline() == 'Log File,Avg rpm\n':
+            csv_file = open(path2csv,'a')
+            print('Header correct.')
+            writer = csv.writer(csv_file)
+        else:
+            print('Header incorrect, starting a new file.')  
+            csv_file = open(path2csv,'w') 
+            writer = csv.writer(csv_file)
+            writer.writerow(['Log File','Avg rpm'])
+    except IOError:
+        print(f'{path2csv} cannot be read or does not exist. Creating a new one.')
+        csv_file = open(path2csv,'w')
+        writer = csv.writer(csv_file)
+        writer.writerow(['Log File','Avg rpm'])
+    else: 
+        csv_file = open(path2csv,'w')
+        writer = csv.writer(csv_file)
+        writer.writerow(['Log File','Avg rpm'])
+    finally:
+        writer.writerow([log_file,rpm])
 
 
-# for logs downloaded with QGC
+
+# file to be tested
 log_path = '/home/lucas/Documents/Log_Analysis/Vibrations/Logs'
 log_index = '270'
 log_date = '2019-8-19'
 log_time = '14-20-40'
-log_file = f'{log_path}/log_{log_index}_{log_date}-{log_time}.ulg'
+test_file = f'{log_path}/log_{log_index}_{log_date}-{log_time}.ulg'
 
-topic_list = []
-topic_list.append('sensor_combined')
-topic_list.append('actuator_outputs')
-topic_list.append('vehicle_local_position_setpoint')
-topic_list.append('vehicle_status')
-topic_list.append('vehicle_land_detected')
-topic_list.append('manual_control_setpoint')
+files = os.listdir(log_path)
 
-info = logextract(log_file,topic_list)
-rpm = info['rpm'] 
-hovering_index = []
-hovering_rpm = []
-
-print(rpm)
-
-for k in range(len(info['time_vs'])):
-    hovering_index.append(ishover(info['navstate'][k],info['stick_in_x'][k],info['stick_in_y'][k],info['stick_in_z'][k]))
-    hovering_rpm.append([rpm[i][k] for i in range(len(rpm)) if hovering_index[k]]) 
-# arrange the sampling time problem
-avg_hovering_rpm = np.mean(hovering_rpm)
-    
+for file in files:
+    log_file = f'{log_path}/{file}'
+    print(log_file)
+    avg_hovering_rpm = avghover(log_file)
+    addrpm(log_file,avg_hovering_rpm)
