354737856675289f46febce2bdf00a3d95447c0e
lucasschn <lucas.schneeberger@gmail.com>
2019-09-11 17:47:31 +0200

    Created notebooks for optimized EC params estimation, OCV curve estimation and Battery simulation. Also changed payload.py to object-oriented program.

--- Hovering/payload.py
+++ Hovering/payload.py
@@ -1,53 +1,59 @@
 import sys
 
 sys.path.append('/home/lucas/Documents/Log_Analysis')
-
-from analog import avghover as avghover, LogError
+import analog
 import numpy as np
 import os
 import csv 
 
-def addhover(log_file,avg_hovering):
-    """ Adds the average hovering rpm results of the file log_file to the file avg_rpm.csv """
-    path2csv = 'avg_hover.csv'
-    try:
-        csv_file = open(path2csv)
-        print(f'{path2csv} has been succesfully opened.')
-        if csv_file.readline() == 'Log File,Avg rpm,Avg vert thrust\n':
-            csv_file = open(path2csv,'a')
-            print('Header correct.')
-            writer = csv.writer(csv_file)
-        else:
-            print('Header incorrect, starting a new file.')  
-            csv_file = open(path2csv,'w') 
-            writer = csv.writer(csv_file)
-            writer.writerow(['Log File','Avg rpm','Avg vert thrust'])
-    except IOError:
-        print(f'{path2csv} cannot be read or does not exist. Creating a new one.')
+
+class Payload_detecter: 
+
+    def __init__(self):
+        self.rpm = []
+        self.thrust = []
+    
+
+    def create_database(self,folder):
+        ''' Reinitilizes the hovering statistics and creates new ones based on all valid logs found in folder.'''
+        self._init()
+        files = os.listdir(folder)
+        for file in files:
+            try :
+                avg_hovering = analog.avghover(log_file)
+                update_database(log_file,avg_hovering)
+                print(f'{log_file} added to database')
+            except LogError:
+                print(f'{log_file} is not relevant. Discarded.')
+                continue
+
+
+    def test_log(self,test_file):
+        payload = False
+        try : 
+            test_results = analog.avghover(test_file)
+            if -100*avg_hovering['zthrust'] > self.mean_trhust + self.std_thrust:
+                payload = True
+            return payload
+        except LogError:
+            print('There is not enough hovering in this file to give results.')
+
+
+    def update_database(self,log_file,avg_hovering):
+        ''' Adds hovering statistics of all valid logs found in folder to the instance attributes.'''
+        if log_file not in self.logfiles:
+            self.rpm.append(rpm)
+            self.thrust.append(thrust)
+            self.mean_rpm = np.mean(self.rpm)
+            self.mean_thrust = np.mean(self.thrust)
+            self.std_thrust = np.std(self.thrust)
+        else: 
+            print(f'{log_file} already found in the database.')
+
+
+    def write_database_to_csv(self,path2csv='avg_hover.csv'):
+        ''' Writes the hovering results to csv file. '''
         csv_file = open(path2csv,'w')
         writer = csv.writer(csv_file)
         writer.writerow(['Log File','Avg rpm','Avg vert thrust'])
-    finally:
-        writer.writerow([log_file,avg_hovering['rpm'],avg_hovering['zthrust']])
-
-
-
-# file to be tested
-log_path = '/home/lucas/Documents/Log_Analysis/Logs'
-log_index = '270'
-log_date = '2019-8-19'
-log_time = '14-33-43'
-test_file = f'{log_path}/log_{log_index}_{log_date}-{log_time}.ulg'
-
-files = os.listdir(log_path)
-
-# database construction
-for file in files:
-    log_file = f'{log_path}/{file}'
-    print(log_file)
-    try :
-        avg_hovering = avghover(log_file)
-    except LogError:
-        print(f'{log_file} is not relevant. Discarded.')
-        continue
-    addhover(log_file,avg_hovering)
+        writer.writerow([self.log_file,self.rpm,self.thrust])
