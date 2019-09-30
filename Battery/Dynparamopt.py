5f62673fcc7e134d2aef369044eca4dd5b94050b
Lucas Schneeberger <Lucas@Oscar-6.local>
2019-09-30 09:54:46 +0200

    Printing evolution of EC dynamic parameters optimization

--- Battery/Dynparamopt.py
+++ Battery/Dynparamopt.py
@@ -2,11 +2,11 @@ import numpy as np
 import pandas as pd
 import os
 import sys
-sys.path.append('/home/lucas/Documents/Log_Analysis/Battery')
+#sys.path.append('/home/lucas/Documents/Log_Analysis/Battery')
+sys.path.append('/Users/Lucas/Documents/Travail/Yuneec/LogAnalysis')
 import analog
 import battery
 from scipy import optimize
-from scipy.optimize import NonlinearConstraint as nlcon
 
 # Battery parameters
 curve = battery.OCVcurve('Battery 9/Discharge 200mA/SOCvsOCV_discharge200mA.csv')
@@ -16,7 +16,8 @@ Q = 6000*3.6
 eta = 1
 
 # define logs to optimize on
-folder = '/home/lucas/Documents/Log_Analysis/Logs/Jack Sparrow (Luigi) z0=1'
+#folder = '/home/lucas/Documents/Log_Analysis/Logs/Jack Sparrow (Luigi) z0=1'
+folder ='/Users/Lucas/Documents/Travail/Yuneec/Logs/Snow Orange (Battery 9) z0=1'
 files = os.listdir(folder)
 
 time = np.array([])
@@ -33,12 +34,6 @@ for file in sorted(files):
     
 n = len(current)
 
-# function to be minimized
-def rmserror(x):
-    y = x[-n:]
-    rmserror = (np.mean((y-voltage)**2))**.5
-    return rmserror
-
 # initial guess
 ECparams = pd.read_csv('ECparams.csv')
 x0 = np.array([ECparams['R0'], ECparams['R1'], ECparams['C1']])
@@ -47,23 +42,49 @@ vsim = initbat.simulate(time,current,curve)
 x0 = np.append(x0,initbat.simz)
 x0 = np.append(x0,initbat.simi1)
 x0 = np.append(x0,initbat.simv)
+prev_x = x0
+print('Inititalizing optimization algortithm ...')
 
+# function to be minimized
+def rmserror(x,voltage,prev_x):
+    y = x[-n:]
+    rmserror = (np.mean((y-voltage)**2))**.5
+    print(x-prev_x)
+    prev_x = x
+    return rmserror
+
+
+def print_res(x):
+    print(x-prev_x)
+    prev_x = x
+    return False
 
 # definition of the constraints
 # x = [R0, R1, C1, z[0], ... , z[n], iR1[0], ..., iR1[n], y[0], ... ,y[n]]
 cons = []
-for k in range(n-1):
+
+def initstate1_eq(x):
+    return x[3]-z0
+
+def initstate2_eq(x):
+    return x[n+4]
+con1 = {'type':'eq','fun':initstate1_eq}   
+con2 = {'type':'eq','fun':initstate2_eq}    
+cons.append(con1)
+cons.append(con2)
+
+for k in range(n):
 
     def state1_eq(x):
-        print('working ...')
+        #print('working ...')
         return x[k+4]-x[k+3]+eta*dt/Q*current[k]
     
     def state2_eq(x):
-        print('working ...')
+        #print('working ...')
         return x[k+(n+1)+4]-np.exp(-dt/(x[1]*x[2]))*x[k+(n+1)+3]-(1-np.exp(-dt/(x[1]*x[2])))*current[k]
     
     def output_eq(x):
-        print('working ...')
+        #print('working ...')
         return curve.OCVfromSOC(x[k+3])-x[1]*x[k+(n+1)+3]-x[0]*current[k]-x[-n+k]
     
     con1 = {'type':'eq','fun':state1_eq}   
@@ -79,7 +100,7 @@ bnd = tuple((0,None) for _ in range(len(x0)))
 dt = np.mean(np.diff(time))
 
 # maximum of iterations
-opt = {'maxiter':30}
-results = optimize.minimize(rmserror,x0,method='SLSQP',constraints=cons,options=opt, bounds=bnd)
+opt = {'maxiter':30,'disp':True}
+results = optimize.minimize(rmserror,x0,args=(voltage, prev_x),method='SLSQP',constraints=cons,options=opt, bounds=bnd, callback=print_res)
 
 print(results)
